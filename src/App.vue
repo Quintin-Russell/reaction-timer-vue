8953c83e6b79b4fd642dc995009a384214f49902
quintin russell <quintinrussell@corporatetools.com>
2022-10-26 09:41:39 -0700

    initial push

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<h1>Reaction Timer</h1>
+<button @click="startGame" :disabled="isPlaying">Play</button>
+<Block v-if="isPlaying" :delay="delay"></Block>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Block from './components/Block.vue'
+
+interface Data {
+  isPlaying: boolean;
+  delay: number|null;
+}
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Block },
+  data(): Data {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    startGame() {
+      this.delay = 1500 + (5000 * Math.random())
+      this.isPlaying = true
+    }
   }
 });
 </script>
@@ -21,7 +37,7 @@ export default defineComponent({
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="block">Click Me!</div>
+</template>
+
+<script lang="ts">
+export default {
+    props: ['delay']
+}
+</script>
+
+<style>
+.block {
+    width: 25%;
+    border-radius: 20%;
+    background-color:rgba(182, 37, 37, 0.556);
+    color: white;
+    padding: 5rem 0;
+    margin: 2rem auto;
+}
+</style>
